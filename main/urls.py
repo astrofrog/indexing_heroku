6d32f6bc7715a4b61ecf5b49b2a2ceb95de60bf0
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-09-18 00:52:59 -0400

    Try and simplify URLs

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     url(r'^add/quantity$', views.add_quantity, name='add_quantity'),
     url(r'^object/(?P<object_name>.+)/$', views.object_view, name='object_view'),
     url(r'^definition/(?P<definition_name>.+)/$', views.definition_view, name='definition_view'),
-    url(r'^object_list/$', views.object_list, name='object_list'),
+    url(r'^$', views.object_list, name='object_list'),
 )
