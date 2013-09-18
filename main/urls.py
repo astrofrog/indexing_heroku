62865b2ec921cebd1c1009633da4b22407b8bd6e
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-09-17 20:56:16 -0400

    Fix URLs

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from main import views
 
 urlpatterns = patterns('',
     url(r'^add/quantity$', views.add_quantity, name='add_quantity'),
-    url(r'^(?P<object_name>.+)/$', views.object_view, name='object_view'),
-    url(r'^(?P<definition_name>.+)/$', views.definition_view, name='definition_view'),
+    url(r'^object/(?P<object_name>.+)/$', views.object_view, name='object_view'),
+    url(r'^definition/(?P<definition_name>.+)/$', views.definition_view, name='definition_view'),
 )
